41c7be3467a78c67f51127f7c249c652ae826ab6
traumarouge <traumarouge@gmail.com>
2019-02-15 20:57:43 +0100

    Migrate build script to Gradle Kotlin DSL / Update JUnit to 5.4.0

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.3.11"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+version = "0.0.1-SNAPSHOT"
+
+val junitJupiterVersion = "5.4.0"
+
+tasks.withType<JavaCompile> {
+    with(options) {
+        compilerArgs.add("-Xlint:deprecation")
+        compilerArgs.add("-Xlint:unchecked")
+        compilerArgs.add("-parameters")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    testImplementation("org.assertj:assertj-core:3.12.0")
+}
